c92cc3a79e8de0b4126fef15f51de8a31841b19f
Tyrone Lu <tyronelu99@gmail.com>
2021-02-02 22:45:55 -0600

    implement search for SongList

--- app/src/main/java/com/simplu/transcribetab/SongListFragment.kt
+++ app/src/main/java/com/simplu/transcribetab/SongListFragment.kt
@@ -4,15 +4,14 @@ import android.Manifest
 import android.content.ContentUris
 import android.content.pm.PackageManager
 import android.database.Cursor
-import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.AdapterView
+import android.widget.FilterQueryProvider
 import android.widget.SimpleCursorAdapter
+import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -22,22 +21,19 @@ import com.simplu.transcribetab.databinding.FragmentSongListBinding
 
 class SongListFragment : Fragment() {
 
-    data class Song(
-        val uri: Uri,
-        val name: String,
-        val duration: Int
-    )
-
     private val PERMISSION_REQUEST_READ_EXTERNAL = 1
 
     private lateinit var binding: FragmentSongListBinding
 
-
+    private lateinit var cursorAdapter: SimpleCursorAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_song_list, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_song_list, container, false
+        )
         permissions()
 
         binding.songList.onItemClickListener = object : AdapterView.OnItemClickListener {
@@ -55,10 +51,17 @@ class SongListFragment : Fragment() {
                     ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, ID)
                         .toString()
 
-                view?.findNavController()?.navigate(SongListFragmentDirections.actionSongListFragmentToEditTabFragment(uri, null))
+                view?.findNavController()?.navigate(
+                    SongListFragmentDirections.actionSongListFragmentToEditTabFragment(
+                        uri,
+                        null
+                    )
+                )
             }
+
         }
 
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return binding.root
     }
@@ -139,15 +142,62 @@ class SongListFragment : Fragment() {
             projection, selection, selectionArgs, sortOrder
         )
 
-        val listItems = intArrayOf(R.id.song_title, R.id.song_duration)
+        val listItems = intArrayOf(
+            R.id.song_title,
+            R.id.song_duration
+        )
         val listColumn: Array<String> =
             arrayOf(
                 MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.DURATION
             )
 
-        val cursorAdapter =
-            SimpleCursorAdapter(context, R.layout.item_song, cursor, listColumn, listItems, 0)
+        cursorAdapter =
+            SimpleCursorAdapter(
+                context,
+                R.layout.item_song, cursor, listColumn, listItems, 0
+            )
+
 
         binding.songList.adapter = cursorAdapter
+
+        binding.songList.isTextFilterEnabled = true
+
+        cursorAdapter.filterQueryProvider = object : FilterQueryProvider {
+            override fun runQuery(constraint: CharSequence?): Cursor {
+                var selection = "title like ?"
+                var selectionArgs = arrayOf("${constraint}%")
+                var sortOrder = null
+
+                val cursor = requireContext().contentResolver.query(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    projection, selection, selectionArgs, sortOrder
+                )
+                return cursor
+            }
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.song_list_menu, menu)
+
+        val item = menu?.findItem(R.id.search)
+        val searchView = item?.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.v("SongListFragment", "text submitted ${query}")
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                cursorAdapter.filter.filter(newText)
+                cursorAdapter.notifyDataSetChanged()
+                return false
+            }
+
+        })
+
     }
 }
